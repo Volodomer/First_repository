b7fad45000846f80611d526456225382845b67ea
Volodomer <112733175+Volodomer@users.noreply.github.com>
2022-09-04 19:33:40 +0300

    Add files via upload
    Sprint_1_hub

---
+++ HT_1_9_Chukavov.py
@@ -0,0 +1,18 @@
+# home work 1_9_Chukavov,
+# Завдання 1
+# Прочитайте у документації з мови Python інформацію про перелічені у резюме цього уроку стандартні функції.
+# Перевірте їх на практиці.
+help(abs)
+
+print(__doc__)
+print.__doc__
+
+help(bool)
+
+help(int)
+
+help(len)
+
+help(map)
+
+help(pow)
